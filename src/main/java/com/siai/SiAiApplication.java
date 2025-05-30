b0f06cc42d12fb58c5ad32ebba3bc628af1bad1b
suhyun <qkrtngus116@naver.com>
2025-05-30 10:03:41 +0900

    init

---
+++ src/main/java/com/siai/SiAiApplication.java
@@ -0,0 +1,13 @@
+package com.siai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siai/SiAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
