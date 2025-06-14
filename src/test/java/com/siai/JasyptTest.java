00195dc9c030ae821c27baa82f8e799b04b4b70c
suhyun <qkrtngus116@naver.com>
2025-06-14 15:54:51 +0900

    test: jasypt test

--- src/test/java/com/siai/JasyptTest.java
+++ src/test/java/com/siai/JasyptTest.java
@@ -7,12 +7,12 @@ public class JasyptTest {
 
 	@Test
 	public void jasyptTest(){
-		String password = "";
+		String password = "sakncksjallkasdkl#$@^#*asdsiajodias2737";
 		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
 		encryptor.setPoolSize(4);
 		encryptor.setPassword(password);
 		encryptor.setAlgorithm("PBEWithMD5AndTripleDES"); // 암호화 알고리즘 설정
-		String content = "root"; // 암호화할 내용
+		String content = "1234"; // 암호화할 내용
 		String encryptedContent = encryptor.encrypt(content); // 암호화
 		String decryptedContent = encryptor.decrypt(encryptedContent);
 		System.out.println("Enc : " + encryptedContent + ", Dec : " + decryptedContent);
